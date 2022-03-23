e4e3a337b7e5b69ae8b59f17a2fff7822ba23a43
jbonner7 <bonnerjoshua@gmail.com>
2022-03-23 13:05:55 -0400

    Create main.tf

---
+++ s3/mfa_delete/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.6.0"
+    }
+  }
+}
+resource "aws_organizations_policy" "scp" {
+  name = "scp_s3"
+  description = "This SCP prevents users or roles in any affected account from deleting any S3 bucket or objects. "
+  content = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "DenyS3DeleteWhenMFAIsNotPresent",
+      "Effect": "Deny",
+      "Action": [
+        "s3:DeleteBucket",
+        "s3:DeleteObject",
+        "s3:DeleteObjectVersion"
+      ],
+      "Resource": "*",
+      "Condition": {"BoolIfExists": {"aws:MultiFactorAuthPresent": false}}
+    }
+  ]
+}
