22ab7a226557a9f83fe2f86c9bf350bf5ba0b35b
jbonner7 <bonnerjoshua@gmail.com>
2022-03-24 09:48:55 -0400

    Update boto3_enable_mfa.py

--- s3/mfa_delete/boto3_enable_mfa.py
+++ s3/mfa_delete/boto3_enable_mfa.py
@@ -3,14 +3,14 @@ from botocore.exceptions import ClientError
 
 s3_client = boto3.client('s3')
 s3_bucket = boto3.resource('s3')
-bucket_name = raw_input('Enter the name of the bucket that you want to enable MFA-delete on: ')
-mfa_token = raw_input('Enter your MFA serial number and token code, e.g. <deviceSerialNumber> <tokenCode>: ')
+bucket_name = input('Enter the name of the bucket that you want to enable MFA-delete on: ')
+mfa_token = input('Enter your MFA serial number and token code, e.g. <deviceSerialNumber> <tokenCode>: ')
 
 try:
     s3_bucket.meta.client.head_bucket(Bucket=bucket_name)
 except ClientError as e:
     if int(e.response['Error']['Code']) == 404:
-        print 'Bucket does not exist'
+        print ('Bucket does not exist')
         exit()
 
 try:
@@ -22,4 +22,4 @@ try:
                                     }
                                 )
 except ClientError as e:
-    print e
+    print (e)
