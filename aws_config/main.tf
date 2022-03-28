2cf99a132aa4de737c3292bdd859eb12a9b723df
jbonner7 <bonnerjoshua@gmail.com>
2022-03-28 11:33:07 -0400

    Update main.tf

--- aws_config/main.tf
+++ aws_config/main.tf
@@ -1,1 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.6.0"
+    }
+  }
+}
 
+
+provider "aws" {
+}
+
+resource "aws_organizations_policy" "scp" {
+  name = "scp_config"
+  description = "This SCP prevents users or roles in any affected account from running AWS Config operations that could disable AWS Config or alter its rules or triggers. "
+  content = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "config:DeleteConfigRule",
+        "config:DeleteConfigurationRecorder",
+        "config:DeleteDeliveryChannel",
+        "config:StopConfigurationRecorder"
+      ],
+      "Resource": "*",
+      "Effect": "Deny"
+    }
+  ]
+}
+POLICY
+
+}
