b8f3183b964bceae5eac8f8c49cf8debdbf40b41
jbonner7 <bonnerjoshua@gmail.com>
2022-10-06 09:27:31 -0400

    folder consolidation

--- s3/bucket_encryption/boto3_enable_s3_bucket_default_encryption.py
+++
@@ -1,7 +0,0 @@
-import boto3
-client = boto3.client('s3')
-
-Buckets = ['bucket-name']
-
-for bucket in Buckets:
-      response = client.put_bucket_encryption(Bucket= bucket, ServerSideEncryptionConfiguration={'Rules': [{'ApplyServerSideEncryptionByDefault': {'SSEAlgorithm': 'AES256'}},]})
